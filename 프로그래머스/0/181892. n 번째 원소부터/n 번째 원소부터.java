b97e2e444ff4b4a1790c0b1605b3c7528b71e8db
SiHyun Kim <fixerpremium@gmail.com>
2025-01-24 23:53:48 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.07 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (int i = n-1; i < num_list.length; i++) {
+            newList.add(num_list[i]);
+        }
+        int[] answer = new int[newList.size()];
+        for (int i = 0; i < newList.size(); i++)
+            answer[i] = newList.get(i);
+        return answer;
+    }
+}
